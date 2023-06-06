33fe1ec443edca39ea93a3deb86d3edc52ebe873
shumpei-kayano <shumpei.kayano@mail.o-hara.ac.jp>
2023-06-06 11:58:17 +0900

    モデルからデータの取得

--- django_blog/blogs/views.py
+++ django_blog/blogs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+
 def index(request):
-    return render(request, 'blogs/index.html')
+    blogs = Blog.objects.order_by('-created_datetime')
+    return render(request, 'blogs/index.html', {'blogs':blogs })
